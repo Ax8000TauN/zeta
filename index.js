458b37f4f190a7b3b197abf596e625a970bc3cbc
Ax8000TauN <33115327+Ax8000TauN@users.noreply.github.com>
2018-05-28 14:49:45 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿function changeBg() {
+  const n = new Date().getHours();
+  const styles = {
+    message: '',
+  };
+  if (n >= 24 && n <= 12) {
+    styles.message = 'Morning';
+  } else if (n >= 12 && n <= 18) {
+    styles.message = 'Afternoon!';
+  } else if (n >= 18 && n <= 24) {
+    styles.message = 'Evening!';
+  }
+  document.querySelector('#message').innerHTML = styles.message;
+}
+
+changeBg();
