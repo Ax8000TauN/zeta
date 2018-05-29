1fcf50588c8a1b857de0a92a22b561ae08a3635d
Ax8000TauN <33115327+Ax8000TauN@users.noreply.github.com>
2018-05-29 08:23:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-﻿function changeBg() {
+﻿function changeTx() {
   const n = new Date().getHours();
   const styles = {
     message: '',
   };
-  if (n >= 24 && n <= 12) {
+  if (n >= 0 && n <= 12) {
     styles.message = 'Morning';
   } else if (n >= 12 && n <= 18) {
     styles.message = 'Afternoon!';
@@ -13,4 +13,4 @@
   document.querySelector('#message').innerHTML = styles.message;
 }
 
-changeBg();
+changeTx();
